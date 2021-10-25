4a917bfaae6516fa9a30918eb6b8a01a29b39525
minerva0394 <58332372+minerva0394@users.noreply.github.com>
2021-10-25 08:34:08 +0800

    10.25课堂练习提交

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-a = -1
-b = bin(a)
-print(b)
-print(~3)
+# 格式化输出
+str = "学生管理系统"
+print('{0:@^30}'.format(str))
+print('{0:*<30}'.format(str))
+print('{0:*>30}'.format(str))
 
-print("尝试提交test")
+# 反转单词输出
+str_input = input("输入一段英文:")
+print("原句子：" + str_input)
+str_tmp = ""
+str_list = str_input.split(" ")
+str_new = list(reversed(str_list))
+for i in str_new:
+    str_tmp = str_tmp + i + " "
+print(str_tmp)
